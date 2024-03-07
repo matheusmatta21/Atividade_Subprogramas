bb272e7ec7a3d38fb5b35cc598ce85420c1fcb00
Matheus Matta <138629402+matheusmatta21@users.noreply.github.com>
2024-03-07 13:01:00 -0300

    formatting

--- db.py
+++ db.py
@@ -1,8 +1,6 @@
 def read_file(path):
     words_list = []
-    with open(path, encoding='utf-8') as file:
+    with open(path, encoding="utf-8") as file:
         for line in file.readlines():
-            words_list.append(line.split(" ")) 
+            words_list.append(line.split(" "))
     return words_list
-
-read_file("a.txt")
