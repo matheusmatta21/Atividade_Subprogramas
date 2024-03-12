8e54248fec990666020c779e7c07dd7729deec9f
Matheus Matta <138629402+matheusmatta21@users.noreply.github.com>
2024-03-12 13:54:55 -0300

    add list comprehension

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 def read_file(path):
     words_list = []
     with open(path, encoding="utf-8") as file:
-        for line in file.readlines():
-            words_list.append(line.split(" "))
+        words_list = [line.split(" ") for line in file.readlines()]
     return words_list
